e8cfaac8d7055dfa2bd684199a99e2f82aefc4c5
Andrii Bardin <andrii.bardin@gmail.com>
2021-01-14 17:56:35 +0200

    added required Dao and Services

--- src/main/java/taxi/model/Car.java
+++ src/main/java/taxi/model/Car.java
@@ -46,4 +46,19 @@ public class Car {
     public List<Driver> getDrivers() {
         return drivers;
     }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "id="
+                + id
+                + ", model='"
+                + model
+                + '\''
+                + ", manufacturer="
+                + manufacturer
+                + ", drivers="
+                + drivers
+                + '}';
+    }
 }
