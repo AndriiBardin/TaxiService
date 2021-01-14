dea020388fb9033ad132a8c733bc62a8f368fa1b
Andrii Bardin <andrii.bardin@gmail.com>
2021-01-14 09:54:11 +0200

    fixed all mistakes

--- src/main/java/taxi/model/Car.java
+++ src/main/java/taxi/model/Car.java
@@ -9,10 +9,10 @@ public class Car {
     private Manufacturer manufacturer;
     private List<Driver> drivers;
 
-    public Car(String model, Manufacturer manufacturer, List<Driver> drivers) {
+    public Car(String model, Manufacturer manufacturer) {
         this.model = model;
         this.manufacturer = manufacturer;
-        this.drivers = new ArrayList<>();
+        drivers = new ArrayList<>();
     }
 
     public void setId(Long id) {
